560959dd5ce780cb063faf5f4c699985aa71a5cb
emincingoz <emincingoz37@gmail.com>
2023-07-12 00:34:33 +0300

    Implementing Order Application Service started with dto and request response objects

---
+++ common/common-domain/src/main/java/com/emin/cingoz/common/domain/valueobject/OrderApprovalStatus.java
@@ -0,0 +1,5 @@
+package com.emin.cingoz.common.domain.valueobject;
+
+public enum OrderApprovalStatus {
+    APPROVED, REJECTED;
+}
---
+++ common/common-domain/src/main/java/com/emin/cingoz/common/domain/valueobject/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.emin.cingoz.common.domain.valueobject;
+
+public enum PaymentStatus {
+    COMPLETED, CANCELLED, FAILED;
+}
